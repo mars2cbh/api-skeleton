f87421f67e7e19a78d0a094654221c301cdfbf3e
David Choi <themars@gmail.com>
2022-01-07 23:36:03 +0900

    indent fix.

--- src/main/java/kr/lasel/apiskeleton/helper/RedisCacheManager.java
+++ src/main/java/kr/lasel/apiskeleton/helper/RedisCacheManager.java
@@ -15,234 +15,234 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class RedisCacheManager {
 
-    @Value("@{spring.redis.prefix}")
-    private String prefix;
-    private static final String CACHE_COUNT_SUFFIX = "_hit";
+  @Value("@{spring.redis.prefix}")
+  private String prefix;
+  private static final String CACHE_COUNT_SUFFIX = "_hit";
 
-    final
-    RedisProperties redisProperties;
+  final
+  RedisProperties redisProperties;
 
-    StatefulRedisConnection<String, String> connection;
-    RedisReactiveCommands<String, String> reactiveCommands;
+  StatefulRedisConnection<String, String> connection;
+  RedisReactiveCommands<String, String> reactiveCommands;
 
-    boolean isAvailable = false;
-    RedisClient redisClient = null;
+  boolean isAvailable = false;
+  RedisClient redisClient = null;
 
-    public RedisCacheManager(RedisProperties redisProperties) {
-        this.redisProperties = redisProperties;
+  public RedisCacheManager(RedisProperties redisProperties) {
+    this.redisProperties = redisProperties;
+  }
+
+  @PostConstruct
+  private void init() {
+
+    String redisUri = String.format("redis://%s:%d/%d",
+        redisProperties.getHost(),
+        redisProperties.getPort(),
+        redisProperties.getDatabase());
+
+    redisClient = RedisClient.create(redisUri);
+  }
+
+  private void reConnect() {
+
+    log.info("Connecting Redis.");
+
+    try {
+      connection = redisClient.connect();
+      reactiveCommands = connection.reactive();
+      log.info("Connect Success.");
+      refresh();
+    } catch (Exception e) {
+      log.error("Redis Connect Error. ");
+    }
+  }
+
+  public void refresh() {
+
+    if (connection == null) {
+      reConnect();
+      return;
     }
 
-    @PostConstruct
-    private void init() {
+    log.debug("Redis Status is {}", connection.isOpen());
 
-        String redisUri = String.format("redis://%s:%d/%d",
-                redisProperties.getHost(),
-                redisProperties.getPort(),
-                redisProperties.getDatabase());
-
-        redisClient = RedisClient.create(redisUri);
+    if (testCache()) {
+      isAvailable = true;
+      log.debug("#### Redis Health Status : [ OK ]");
+    } else {
+      isAvailable = false;
+      log.debug("#### Redis Health Status : [ ERROR ]");
     }
+  }
 
-    private void reConnect() {
-
-        log.info("Connecting Redis.");
-
-        try {
-            connection = redisClient.connect();
-            reactiveCommands = connection.reactive();
-            log.info("Connect Success.");
-            refresh();
-        } catch (Exception e) {
-            log.error("Redis Connect Error. ");
-        }
+  public void setCacheData(String cacheKey, String value, int expireSecond) {
+    try {
+      setCache(cacheKey, expireSecond, value);
+      setCache(generateCacheCountKey(cacheKey), expireSecond, "0");
+    } catch (Exception e) {
+      log.debug("", e);
     }
+  }
 
-    public void refresh() {
+  public String getCacheData(String key, int expireCount) {
 
-        if (connection == null) {
-            reConnect();
-            return;
-        }
+    String result = null;
 
-        log.debug("Redis Status is {}", connection.isOpen());
+    try {
+      if (existCache(key)) {
+        long cacheCount = incrementCache(generateCacheCountKey(key));
+        log.debug("[#Cache] get cache hit count  : {}", cacheCount);
 
-        if (testCache()) {
-            isAvailable = true;
-            log.debug("#### Redis Health Status : [ OK ]");
+        if (cacheCount >= expireCount) {
+          log.debug("[#Cache] Deleted Key : {}", key);
+          removeCache(key);
         } else {
-            isAvailable = false;
-            log.debug("#### Redis Health Status : [ ERROR ]");
+          result = getCache(key);
         }
+      }
+    } catch (Exception e) {
+      log.error(e.getMessage());
     }
 
-    public void setCacheData(String cacheKey, String value, int expireSecond) {
-        try {
-            setCache(cacheKey, expireSecond, value);
-            setCache(generateCacheCountKey(cacheKey), expireSecond, "0");
-        } catch (Exception e) {
-            log.debug("", e);
-        }
+    return result;
+  }
+
+  public long incrementCache(String key) {
+
+    if (!connection.isOpen()) {
+      return 0;
     }
 
-    public String getCacheData(String key, int expireCount) {
+    Long result = reactiveCommands.incr(key).block();
+    if (result != null) {
+      return result;
+    } else {
+      return 0;
+    }
+  }
 
-        String result = null;
-
-        try {
-            if (existCache(key)) {
-                long cacheCount = incrementCache(generateCacheCountKey(key));
-                log.debug("[#Cache] get cache hit count  : {}", cacheCount);
-
-                if (cacheCount >= expireCount) {
-                    log.debug("[#Cache] Deleted Key : {}", key);
-                    removeCache(key);
-                } else {
-                    result = getCache(key);
-                }
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-
-        return result;
+  public long removeCache(String key) {
+    if (!connection.isOpen()) {
+      return 0;
     }
 
-    public long incrementCache(String key) {
+    Long result = reactiveCommands.del(key).block();
 
-        if (!connection.isOpen()) {
-            return 0;
-        }
+    if (result != null) {
+      return result;
+    } else {
+      return 0;
+    }
+  }
 
-        Long result = reactiveCommands.incr(key).block();
-        if (result != null) {
-            return result;
-        } else {
-            return 0;
-        }
+  public boolean existCache(String key) {
+    if (!connection.isOpen()) {
+      return false;
     }
 
-    public long removeCache(String key) {
-        if (!connection.isOpen()) {
-            return 0;
-        }
+    Long exist = reactiveCommands.exists(key).block();
 
-        Long result = reactiveCommands.del(key).block();
-
-        if (result != null) {
-            return result;
-        } else {
-            return 0;
-        }
+    if (exist != null && exist > 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public boolean existCache(String key) {
-        if (!connection.isOpen()) {
-            return false;
-        }
-
-        Long exist = reactiveCommands.exists(key).block();
-
-        if (exist != null && exist > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // result cache key gen
+  // result cache key gen
 //    public String generateCacheKey(String string) {
 //        String uuid = DigestUtils.md5DigestAsHex(string.getBytes(StandardCharsets.UTF_8)).toUpperCase();
 //        return prefix + uuid;
 //    }
 
-    // hit count cache key gen
-    public String generateCacheCountKey(String key) {
-        return key + CACHE_COUNT_SUFFIX;
+  // hit count cache key gen
+  public String generateCacheCountKey(String key) {
+    return key + CACHE_COUNT_SUFFIX;
+  }
+
+  private boolean setCache(String key, String value) {
+
+    String result = null;
+
+    if (!connection.isOpen()) {
+      return false;
     }
 
-    private boolean setCache(String key, String value) {
+    try {
+      result = reactiveCommands.set(key, value)
+          .block();
+    } catch (Exception e) {
+      log.debug("", e);
+    }
+    return "ok".equalsIgnoreCase(result);
+  }
 
-        String result = null;
+  private boolean setCache(String key, int expireSecond, String value) {
+    String result = null;
 
-        if (!connection.isOpen()) {
-            return false;
-        }
-
-        try {
-            result = reactiveCommands.set(key, value)
-                    .block();
-        } catch (Exception e) {
-            log.debug("", e);
-        }
-        return "ok".equalsIgnoreCase(result);
+    if (!connection.isOpen()) {
+      return false;
     }
 
-    private boolean setCache(String key, int expireSecond, String value) {
-        String result = null;
-
-        if (!connection.isOpen()) {
-            return false;
-        }
-
-        try {
-            result = reactiveCommands.setex(key, expireSecond, value).block();
-        } catch (Exception e) {
-            log.debug("", e);
-        }
-
-        if ("ok".equalsIgnoreCase(result)) {
-            log.debug("[#Cache] set cache : {}", key);
-            return true;
-        } else {
-            log.debug("[#Cache] Failed to set cache! : {}", key);
-            return false;
-        }
+    try {
+      result = reactiveCommands.setex(key, expireSecond, value).block();
+    } catch (Exception e) {
+      log.debug("", e);
     }
 
-    public String getCache(String key) {
-        String result = null;
+    if ("ok".equalsIgnoreCase(result)) {
+      log.debug("[#Cache] set cache : {}", key);
+      return true;
+    } else {
+      log.debug("[#Cache] Failed to set cache! : {}", key);
+      return false;
+    }
+  }
 
-        if (!connection.isOpen()) {
-            return null;
-        }
+  public String getCache(String key) {
+    String result = null;
 
-        try {
-            result = reactiveCommands.get(key).block();
-        } catch (Exception e) {
-            log.debug("", e);
-        }
-
-        return result;
+    if (!connection.isOpen()) {
+      return null;
     }
 
-    private boolean testCache() {
-        boolean result = false;
-
-        try {
-            setCache("ping", "pong");
-
-            String str = getCache("ping");
-            if (str != null && str.equals("pong")) {
-                result = true;
-            }
-
-        } catch (Exception e) {
-            log.warn("TestCache : ", e);
-        }
-
-        return result;
-
+    try {
+      result = reactiveCommands.get(key).block();
+    } catch (Exception e) {
+      log.debug("", e);
     }
 
-    @Scheduled(cron = "0 * * * * *")
-    public void scheduleRefresh() throws Exception {
-        log.debug("##### CacheHealthChecker(scheduler) : check redis health ...");
-        refresh();
+    return result;
+  }
+
+  private boolean testCache() {
+    boolean result = false;
+
+    try {
+      setCache("ping", "pong");
+
+      String str = getCache("ping");
+      if (str != null && str.equals("pong")) {
+        result = true;
+      }
+
+    } catch (Exception e) {
+      log.warn("TestCache : ", e);
     }
 
-    @PreDestroy
-    private void destroy() {
-        connection.close();
-    }
+    return result;
+
+  }
+
+  @Scheduled(cron = "0 * * * * *")
+  public void scheduleRefresh() throws Exception {
+    log.debug("##### CacheHealthChecker(scheduler) : check redis health ...");
+    refresh();
+  }
+
+  @PreDestroy
+  private void destroy() {
+    connection.close();
+  }
 
 }
